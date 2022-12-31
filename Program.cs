cc00ec8e387894d2c5165e09cdbf198fd856ce4b
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-31 18:49:32 +0530

    updated for UC1

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ class Program
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcme to Line Comparison Program !!");
-
+            LineLength obj1 = new LineLength();
+            obj1.cal_len();
             
         }
     }
