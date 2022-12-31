7a34bf553a9bcc880d4bcfcb279f72f055db9da6
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2022-12-31 18:59:14 +0530

    updated for uc3

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@ class Program
         public static void Main(string[] args)
         {
           Console.WriteLine("Welcme to Line Comparison Program !!");
-          lineEqual ob = new lineEqual();
-          ob.equal();
+          lineCompare obj = new lineCompare();
+            obj.line_compare();
             
         }
     }
